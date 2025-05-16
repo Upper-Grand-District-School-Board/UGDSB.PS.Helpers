2c40d856fe7674abb8a81777ce6437f2dff9c2a2
Jeremy Putman <jeremy.putman@ugdsb.on.ca>
2025-05-16 14:08:47 -0400

    Change to the display components of dashboard

--- cmdlets/public/PSU/ManageMailboxPermissions/Get-PermissionList.ps1
+++ cmdlets/public/PSU/ManageMailboxPermissions/Get-PermissionList.ps1
@@ -6,14 +6,10 @@ function Get-PermissionList {
   )
   switch ($permissionType) {
     "fullaccess" {
-      $list = Get-MailboxPermission -Identity $mailbox | Where-Object { $_.User -ne "NT AUTHORITY\SELF" } | Select-Object -Property User, AccessRights, Action
-      $columdata = @(    
-        @{ field = "user"; flex = 1.0;}
-      )
+      $list = Get-MailboxPermission -Identity $mailbox | Where-Object { $_.User -ne "NT AUTHORITY\SELF" } | Select-Object -Property User
     }
     "sendas" {
-      $list = Get-RecipientPermission -Identity $mailbox | Where-Object { $_.Trustee -ne "NT AUTHORITY\SELF" } | Select-Object -Property Trustee, AccessRights, IsInherited
-      $columdata = @(    @{ field = "Trustee"; flex = 1.0 }) 
+      $list = Get-RecipientPermission -Identity $mailbox | Where-Object { $_.Trustee -ne "NT AUTHORITY\SELF" } | Select-Object -Property Trustee
     }
     "sendonbehalf" {
       $list = [System.Collections.Generic.List[PSCustomObject]]::new()
@@ -22,27 +18,18 @@ function Get-PermissionList {
         $graphUser = Get-GraphUser -userid $obj
         $list.Add([PSCustomObject]@{ User = $graphUser.DisplayName; Email = $graphUser.UserPrincipalName })
       }   
-      $columdata = @(
-        @{ field = "User"; flex = 1.0 }
-        @{ field = "Email"; flex = 1.0 }
-      )   
     }
     "calendaraccess" {
       $list = [System.Collections.Generic.List[PSCustomObject]]::new()
       $calendarPermissions = Get-MailboxFolderPermission -Identity "$($mailbox):\Calendar" | Select-Object -Property User, AccessRights
       foreach ($obj in $calendarPermissions) {
         $list.Add([PSCustomObject]@{ User = $obj.User.DisplayName; AccessRights = ($obj.AccessRights -join ",") })
-      }  
-      $columdata = @(
-        @{ field = "User"; flex = 1.0 }
-        @{ field = "AccessRights"; flex = 1.0 }	
-      )           
+      }         
     }
   }
   Set-UDElement -Id $permissionType -Content {
     if($list){
-    New-UDDataGrid -id "$($permissionType)_grid" -LoadRows {
-      $list | Out-UDDataGridData -Context $EventData -TotalRows $list.Count
-    } -Columns $columdata -AutoHeight $true -PageSize 500         }
+      New-UDTable -Data $list
+    }
   }
 }
